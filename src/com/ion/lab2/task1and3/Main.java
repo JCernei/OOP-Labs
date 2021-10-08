bd894f6a3d59d237e27de9df13de36f6fce43e0a
JCernei <jonnycernei@gmail.com>
2021-10-09 00:56:12 +0300

    add lab2

---
+++ src/com/ion/lab2/task1and3/Main.java
@@ -0,0 +1,13 @@
+package com.ion.lab2.task1and3;
+
+public class Main {
+    public static void main(String[] args) {
+        Box box1 = new Box();
+        Box box2 = new Box(5);
+        Box box3 = new Box(5, 4, 3);
+
+        System.out.println(String.valueOf(box1.getSurface()) + ' ' + String.valueOf(box1.getVolume()));
+        System.out.println(String.valueOf(box2.getSurface()) + ' ' + String.valueOf(box2.getVolume()));
+        System.out.println(String.valueOf(box3.getSurface()) + ' ' + String.valueOf(box3.getVolume()));
+    }
+}
