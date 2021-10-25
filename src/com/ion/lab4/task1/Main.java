ac9be43a084979b4c3cb5ae345584da548d0d73c
JCernei <jonnycernei@gmail.com>
2021-10-25 23:25:26 +0300

    Add lab4

---
+++ src/com/ion/lab4/task1/Main.java
@@ -0,0 +1,22 @@
+package com.ion.lab4.task1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        List<String> linesF1 = Files.readAllLines(Paths.get("resources/one_expression.txt"));
+        System.out.println("1 expression file:");
+        for (String line : linesF1) {
+            System.out.println(ExpChecker.isCorrect(line));
+        }
+
+        List<String> linesF2 = Files.readAllLines(Paths.get("resources/three_expressions.txt"));
+        System.out.println("3 expressions file:");
+        for (String line : linesF2) {
+            System.out.println(ExpChecker.isCorrect(line));
+        }
+    }
+}
